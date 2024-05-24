a50e9827e366ea0425c0c5179e0aea9322c955d7
LHH <dlgsughk1210@naver.com>
2024-05-24 16:33:59 +0900

    3st

--- js/api.js
+++ js/api.js
@@ -1,3 +1,18 @@
+const koreaMovies = Array.from(
+  document.querySelectorAll("#section1 .korea > li")
+);
+const globalMovies = Array.from(
+  document.querySelectorAll("#section2 .global > li")
+);
+const prevBtn = document.querySelector("#section1 .prev-btn");
+const nextBtn = document.querySelector("#section1 .next-btn");
+const prevBtn2 = document.querySelector("#section2 .prev-btn");
+const nextBtn2 = document.querySelector("#section2 .next-btn");
+
+let startIndex = 0; // 한국 영화의 시작 인덱스
+let startIndex2 = 0; // 해외 영화의 시작 인덱스
+
+// 영화 api 가져오기
 async function fetchMovies(url) {
   const response = await fetch(url);
   const data = await response.json();
@@ -35,3 +50,43 @@ async function displayMovies(type) {
 
 displayMovies("korea");
 displayMovies("global");
+
+// 인덱스 인기차트
+function showMovies(movies, startIndex) {
+  movies.forEach((movie, index) => {
+    if (index >= startIndex && index < startIndex + 4) {
+      movie.style.display = "block"; // 보이기
+    } else {
+      movie.style.display = "none"; // 숨기기
+    }
+  });
+}
+
+showMovies(koreaMovies, startIndex);
+showMovies(globalMovies, startIndex2);
+
+nextBtn.addEventListener("click", function () {
+  startIndex += 4; // 다음 4개의 한국 영화를 보여줌
+  showMovies(koreaMovies, startIndex);
+});
+
+prevBtn.addEventListener("click", function () {
+  startIndex -= 4; // 이전 4개의 한국 영화를 보여줌
+  if (startIndex < 0) {
+    startIndex = 0; // 음수가 되지 않도록 보정
+  }
+  showMovies(koreaMovies, startIndex);
+});
+
+nextBtn2.addEventListener("click", function () {
+  startIndex2 += 4; // 다음 4개의 해외 영화를 보여줌
+  showMovies(globalMovies, startIndex2);
+});
+
+prevBtn2.addEventListener("click", function () {
+  startIndex2 -= 4; // 이전 4개의 해외 영화를 보여줌
+  if (startIndex2 < 0) {
+    startIndex2 = 0; // 음수가 되지 않도록 보정
+  }
+  showMovies(globalMovies, startIndex2);
+});
